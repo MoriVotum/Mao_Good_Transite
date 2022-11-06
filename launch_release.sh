89eca9e8ba9f8ccf5f364068f9864430d34b6cd6
Desire <nacbboom@gmail.com>
2022-11-07 01:15:01 +0300

    init_2

--- launch_release.sh
+++
@@ -1,8 +0,0 @@
-SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
-MAIN="$SCRIPT_DIR/bin/Mao_Good_Transite_x64.dll"
-export LD_LIBRARY_PATH="$SCRIPT_DIR/bin:$LD_LIBRARY_PATH"
-if [ -f "$MAIN" ]; then
-	dotnet "$MAIN" 
-else
-	echo "Application executable not found"
-fi
